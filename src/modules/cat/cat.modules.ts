a089d587750f7777492c398ada76e0119e78ca79
linxiaodi <343206347@qq.com>
2021-03-04 11:44:12 +0800

    feat: init

--- src/modules/cat/cat.modules.ts
+++ src/modules/cat/cat.modules.ts
@@ -1,11 +1,15 @@
-import { Global, Module } from '@nestjs/common';
+import { Global, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CatService } from './cat.service';
 import { CatController } from './cat.controller';
+import { LogMiddleware } from './log.middleware';
 
 @Module({
   providers: [CatService],
   controllers: [CatController],
   exports: [CatService]
 })
-export class CatModules {
+export class CatModules implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    return consumer.apply(LogMiddleware).forRoutes('cat/say')
+  }
 }
